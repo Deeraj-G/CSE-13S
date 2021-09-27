2fe6de61b2531b3587d8c71ab4163e804bfbd27c
Deeraj Gurram <dgurram@ucsc.edu>
2021-09-27 22:08:43 +0000

    Created pig.c, defined the pig array

---
+++ asgn1/pig.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+typedef  enum { SIDE , RAZORBACK , TROTTER , SNOUTER , JOWLER } Position;
+const Position pig[7] = {
+	SIDE,
+	SIDE,
+	RAZORBACK,
+	TROUTER,
+	SNOUTER,
+	JOWLER,
+	JOWLER
+};
+

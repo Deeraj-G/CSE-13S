3061ad381a3e7d3489f39a62bf59c7765651e3d5
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-07 05:38:45 +0000

    Finished code for e.c and newton.c

--- asgn2/e.c
+++ asgn2/e.c
@@ -1,19 +1,20 @@
+#include "mathlib.h"
+#include <stdio.h>
+static int count;
 
 // Computes euler's number and finds the number of terms that were computed
-int main() {
-	int e() {
-	
+double e(void) {
+    double factorial = 1.0;
+    double n = 1.0;
+    count = 0;
+    for (int i = 0; (1/factorial) > EPSILON; i++) {
+        factorial = factorial * (i + 1);
+        n = n + (1 / factorial);
+        count = i;
+    }
+    return n;
+}
 
-	}
-
-	int e_terms() {
-
-
-
-	}
-
-
-
-
-
+int e_terms(void) {
+	return count;
 }
---
+++ asgn2/newton.c
@@ -0,0 +1,19 @@
+#include "mathlib.h"
+#include <stdio.h>
+static int newt_count;
+
+double sqrt_newton(double x) {
+	double z = 0.0;
+	double y = 1.0;
+	newt_count = 0;
+	while (absolute(y - z) > EPSILON) {
+		z = y;
+		y = 0.5 * (z + x / z);
+		newt_count += 1;
+	}
+	return y;
+    }
+
+int sqrt_newton_iters() {
+	return newt_count;
+}

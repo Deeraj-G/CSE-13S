87cf71d67e294088d28eabb8cbd1ec148f56f1f5
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-07 06:31:09 +0000

    Created the diff for -e and -n

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -10,20 +10,31 @@ int main(int argc, char **argv) {
     int opt = 0;
     while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
         switch (opt) {
+	case 'a': ;
+		break;
         case 'e': ;
 		double e_test;
 		e_test = e();
-        	printf("e(): %16.15lf, M_E: %16.15lf\n", e_test, M_E);
+		double e_diff;
+		e_diff = absolute(e_test - M_E);
+        	printf("e() = %16.15lf, M_E = %16.15lf, diff = %16.15lf\n", e_test, M_E, e_diff);
         	break;
+	case 'b': ;
+		break;
+	case 'm': ;
+		break;
+	case 'r': ;
+		break;
+	case "v': ;
+		break;
 	case 'n': ;
 		double n_diff = 0.0;
 		double newt_test;
-		double l = 0.0;
-		newt_test = sqrt_newton(l);
-		for (l; l < 10.0; l = l + 0.1) {
-			n_diff = absolute(newt_test - sqrt(l));
-			return n_diff;
-			printf("sqrt(): %16.15f, Actual sqrt: %16.15f, diff: %16.15lf\n", newt_test, sqrt(25.0), n_diff);
+		double t = 0.0;
+		for (t; t < 10.0; t = t + 0.1) {
+			newt_test = sqrt_newton(t);
+			n_diff = absolute(newt_test - sqrt(t));
+			printf("sqrt_newton(%f) =  %16.15f, sqrt(%f) = %16.15f, diff = %16.15lf\n", newt_test, t, sqrt(t), t, n_diff);
 		}
 		break;
 	case 's': ;
@@ -32,6 +43,8 @@ int main(int argc, char **argv) {
 		int newt_terms;
 		newt_terms = sqrt_newton_iters();
 		printf("e terms: %d, newton terms: %d\n", e_count, newt_terms);
+		break;
+	case 'h': ;
 		break;
 	default:
 	    break;

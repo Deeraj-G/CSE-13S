3fc57158629c26a4d8bab2c70a4256f15063a9f5
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-09 04:12:40 +0000

    euler.c only goes to 46000 terms, trying to fix it

--- asgn2/euler.c
+++ asgn2/euler.c
@@ -1,28 +1,26 @@
 #include "mathlib.h"
 #include <stdio.h>
+#include <inttypes.h>
 static int f;
 
 double pi_euler(void) {
 	double current = 1.0;
 	double sum = 0.0;
-	int temp;
-	for (f = 1; current >= EPSILON; f++) {
+	double temp;
+
+	for (f = 1; current > EPSILON; f++) {
 		temp = f * f;
-		printf("temp: %d", temp);
-		current = 1 / temp;
+		current = 1.0 / temp;
 		sum += current;
-		printf("loop sum: %lf\n", sum);
 	}
 
-	sum = sum * 6;
-	printf("%lf\n", sum);
+	sum *= 6;
 	sum = sqrt_newton(sum);
-	printf("sqrted: %lf\n", sum);
 	return sum;
 }
 
 
 
 int pi_euler_terms(void) {
-	return f-1;
+	return f;
 }

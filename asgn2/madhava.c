f114824e33f3fc42c0647132b5c30f02aa0def98
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-09 06:54:58 +0000

    Finished madhava.c

--- asgn2/madhava.c
+++ asgn2/madhava.c
@@ -6,16 +6,16 @@ double pi_madhava(void) {
 	double current = 1.0;
 	double sum = 0.0;
 	double exp = -3.0;
-	for (g = 0; current > EPSILON; g++) {
+	for (g = 0; absolute(current) > EPSILON; g++) {
 		if (g > 0) {
 			current = 1.0;
-			current = current / exp;
-			exp = exp * -3.0;
+			current /= exp;
+			exp *= -3.0;
 		}
-		current = current / (2 * g) + 1;
+		current /= (2 * g) + 1;
 		sum += current;
 	}
-	sum = sum * sqrt_newton(12);
+	sum *= sqrt_newton(12);
 	return sum;
 }
 

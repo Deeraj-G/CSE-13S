8a18b53f84c9eb82140b76441b2f78119b843e94
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-09 06:17:28 +0000

    Finished viete.c

--- asgn2/viete.c
+++ asgn2/viete.c
@@ -3,17 +3,15 @@
 static int d;
 
 double pi_viete(void) {
-	double current = 3.0;
+	double current = 0.0;
 	double product = 1.0;
 	double repeat = sqrt_newton(2);
 
-	for (d = 1; current > 1 - current; d++) {
+	for (d = 1; current < 1 - EPSILON; d++) {
 		current = repeat / 2;
 		product *= current;
 		repeat = sqrt_newton(2 + repeat);
-		printf("repeat: %lf, current: %lf, product: %lf\n", repeat, current, product);
 	}
-	printf("%lf\n", product);	
 	product = 2 / product;
 	return product;
 }

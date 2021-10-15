4d430388b72246772e4c04ce63ccc6b33f643e4a
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-15 00:08:50 +0000

    Finished most of insert.c, running into a couple bugs

--- asgn3/insert.c
+++ asgn3/insert.c
@@ -1,18 +1,33 @@
 #include "insert.h"
 #include <stdint.h>
 #include "stats.h"
+#include <inttypes.h>
 
-
+// Implement the quick sort algorithm
 void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
-	int j;
-	int temp;
-	for (int i = 1; i < sizeof(A); i++) {
+	uint32_t j;
+	uint32_t temp;
+	// We compare the current element to the previous element, so i must start at 1
+	for (uint32_t i = 1; i < n; i++) {
+		
 		j = i;
 		temp = A[i];
-		while ( j > 0 && temp < A[j - 1]) {
+		
+		// j must be > 0 because it would give an out of range error otherwise
+		// If temp < A[j - 1] then the current element must be moved back in the array
+		
+		while ( j > 0 && cmp(stats, temp, A[j - 1] > 0)) {
+
+			// Set the current element equal to the larger value	
 			A[j] = A[j - 1];
+			move(stats, A[j]);
+
+			// Check the next previous element
 			j -= 1;
+		
 		}
+		// Set the lower element equal to the smaller value
 		A[j] = temp;
+		move(stats, A[j]);
 	}
 }

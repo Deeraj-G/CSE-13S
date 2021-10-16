169ea6cf6d9cf6e2e9aae2deaa96cf576425ca5d
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-16 23:04:20 +0000

    Fixed most of shell.c, need to get the compares and moves working properly

--- asgn3/shell.c
+++ asgn3/shell.c
@@ -5,19 +5,35 @@
 #include <stdio.h>
 #include <math.h>
 
+
+int gaps (uint32_t n) {
+        
+        uint32_t gap_length = log(3 + 2 * n) / log(3);
+       	uint32_t var; 
+        for (uint32_t i = gap_length; i > 0; i -= 1) {
+                if (i > 0) {
+			var = ((pow(3, i) - 1) / 2);
+                        return var;
+                }
+        }
+}
+
+
+
 void shell_sort(Stats *stats, uint32_t *A, uint32_t n) {
 
 	uint32_t j;
 	uint32_t temp;
+	uint32_t func = gaps(n);
 
 	for (int i = 0; i < n; i++) {
-		for ( int l = gap_length; l < n; l -= 1) {
+		for ( int l = func; l < n; l -= 1) {
 			j = l;
 			temp = A[l];
-			while (j >= gap_length && temp < A[j - gap_length]) {
-				A[j] = A[j - gap_length];
+			while (j >= func  && temp < A[j - func]) {
+				A[j] = A[j - func];
 				move(stats, A[j]);
-				j -= gap_length;
+				j -= func;
 			}
 		A[j] = temp;
 		move(stats, A[j]);
@@ -25,13 +41,3 @@ void shell_sort(Stats *stats, uint32_t *A, uint32_t n) {
 		}
 	}
 }
-
-void gaps (uint32_t n) {
-	uint32_t gap_length = log(3 + 2 * n) / log(3);
-	for (uint32_t i = gap_length; i > 0; i -= 1) {
-		if (i > 0) {
-			return ((pow(3, i) - 1) / 2) ;
-		}
-	}
-}
-

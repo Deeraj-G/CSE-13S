91f77d77d120e1cd38fa58854a13e6eb8f442fd2
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-16 06:10:43 +0000

    Translated pseudocode to C

--- asgn3/shell.c
+++ asgn3/shell.c
@@ -3,22 +3,35 @@
 #include "stats.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <math.h>
 
 void shell_sort(Stats *stats, uint32_t *A, uint32_t n) {
 
-	
+	uint32_t j;
+	uint32_t temp;
 
+	for (int i = 0; i < n; i++) {
+		for ( int l = gap_length; l < n; l -= 1) {
+			j = l;
+			temp = A[l];
+			while (j >= gap_length && temp < A[j - gap_length]) {
+				A[j] = A[j - gap_length];
+				move(stats, A[j]);
+				j -= gap_length;
+			}
+		A[j] = temp;
+		move(stats, A[j]);
 
-
-
-
+		}
+	}
 }
 
 void gaps (uint32_t n) {
-	for (uint32_t i = log(3 + 2 * n) / log(3); i > 0; i -= 1) {
+	uint32_t gap_length = log(3 + 2 * n) / log(3);
+	for (uint32_t i = gap_length; i > 0; i -= 1) {
 		if (i > 0) {
-			uint32_t j *= i;
-			// find out how to yield
-			return ((3 * j) - 1) / 2;
+			return ((pow(3, i) - 1) / 2) ;
 		}
+	}
 }
+

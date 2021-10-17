e5b1435742d75c4d3b64362721e213cf78ad37ea
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-17 04:43:03 +0000

    Added case for -h

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -41,7 +41,8 @@ int main(int argc, char **argv) {
 	while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
 		switch(opt) {
 		case 'a':
-                        s = insert_set(ALL, s);
+			reset(&stats);
+			s = insert_set(ALL, s);
                         break;
 		case 'i':
 			reset(&stats);
@@ -62,6 +63,22 @@ int main(int argc, char **argv) {
 			reset(&stats);
 			s = insert_set(QUICK, s);
 			quick_sort(&stats, A, elements);
+			break;
+		case 'h':
+			printf("SYNOPSIS\n");
+			printf("   A collection of comparison-based sorting algorithms.\n\n");
+			printf("USAGE\n");
+			printf("   ./sorting [-haeisqn:p:r:] [-n length] [-p elements] [-r seed]\n\n");
+			printf("OPTIONS\n");
+			printf("   -h              display program help and usage.\n");
+			printf("   -a              enable all sorts.\n");
+			printf("   -e              enable Heap Sort.\n");
+			printf("   -i              enable Insertion Sort.\n");
+			printf("   -s              enable Shell Sort.\n");
+			printf("   -q              enable Quick Sort.\n");
+			printf("   -n length       specify number of array elements (default: 100).\n");
+			printf("   -p elements     specify number of elements to print (default: 100).\n");
+			printf("   -r seed         specify random seed (default: 13371453).\n");
 			break;
 		case 'n':
 			break;
@@ -97,7 +114,7 @@ int main(int argc, char **argv) {
         }
 		}
 	}
-
+	free(A);
 	return 0;
 		
 }

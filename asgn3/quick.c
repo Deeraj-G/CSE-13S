bb2aa23b15c5dd5c1774bb815445fae7029279b9
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-17 01:04:16 +0000

    Completed quick.c

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -8,32 +8,28 @@
 
 uint32_t partition(Stats *stats, uint32_t *A,  uint32_t lo, uint32_t hi) {
 	uint32_t i = lo - 1;
-	uint32_t temp;
-	uint32_t temp2 = A[i];
 
 	for (uint32_t j = lo; j < hi; j++) {
-		i += 1;
-		temp = A[i - 1];
-
-		A[i - 1] = A[j - 1];
-		A[j - 1] = temp;
+		if ((cmp(stats, A[j - 1], A[hi - 1])) < 0) {
+			i += 1;
+			swap(stats, &A[i - 1], &A[j - 1]);
+		}
 	}
 
-	A[i] = A[hi - 1];
-	A[hi - 1] = temp2;
+	swap(stats, &A[i], &A[hi - 1]);
 	return i + 1;
 }
 
 void quick_sorter(Stats *stats, uint32_t *A,  uint32_t lo, uint32_t hi) {
 	if (lo < hi) {
-		uint32_t p = partition(A, lo, hi);
-		quick_sorter(A, lo, p - 1);
-		quick_sorter(A, p + 1, hi);
+		uint32_t p = partition(stats, A, lo, hi);
+		quick_sorter(stats, A, lo, p - 1);
+		quick_sorter(stats, A, p + 1, hi);
 	}
 }
 
 void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
-	quick_sorter(A, 1, n);
-
-
+	quick_sorter(stats, A, 1, n);
 }
+
+

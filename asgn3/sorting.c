dff7b777971087eedd31f19cdc474be6667da18e
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-17 04:31:30 +0000

    Fixed bit mask and made more cases

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -16,7 +16,7 @@
 #define OPTIONS "haeisqnpr"
 
 
-typedef enum { INSERTION, SHELL, HEAP, QUICK, NUM_SORTS } Sorts;
+typedef enum { ALL, INSERTION, SHELL, HEAP, QUICK, HELP, NUM_SORTS } Sorts;
 const char *names[] = { "shell sort", "insertion sort", "heap sort", "quick sort" };
 
 
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
 	stats.compares = 0;
 	uint32_t seed = 13371453;	
 	uint32_t elements = 100;
-	uint32_t mask = 0xFFFFFFFE;
+	uint32_t mask = 0x3FFFFFFF;
 
 	srandom(seed);
 
@@ -41,24 +41,25 @@ int main(int argc, char **argv) {
 	while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
 		switch(opt) {
 		case 'a':
-                        s = insert_set(INSERTION, s);
-			s = insert_set(SHELL, s);
-			s = insert_set(HEAP, s);
-			s = insert_set(QUICK, s);
+                        s = insert_set(ALL, s);
                         break;
 		case 'i':
+			reset(&stats);
 			s = insert_set(INSERTION, s);
 			insertion_sort(&stats, A, elements);
 			break;
 		case 's':
+			reset(&stats);
                         s = insert_set(SHELL, s);
 			shell_sort(&stats, A, elements);
                         break;
 		case 'e':
+			reset(&stats);
                         s = insert_set(HEAP, s);
 			heap_sort(&stats, A, elements);
                         break;
 		case 'q':
+			reset(&stats);
 			s = insert_set(QUICK, s);
 			quick_sort(&stats, A, elements);
 			break;
@@ -68,9 +69,32 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	for (Sorts x = INSERTION; x < NUM_SORTS; x++) {
+	for (Sorts x = ALL; x < NUM_SORTS; x++) {
 		if (member_set(x, s)) {
-			printf("%s is part of the set\n", names[x]);
+			
+			if (x == INSERTION) {
+				printf("Insertion Sort, ");
+			}
+			else if (x == SHELL) {
+				printf("Shell Sort, ");
+			}
+			else if (x == HEAP) {
+				printf("Heap Sort, ");
+			}
+			else if (x == QUICK) {
+				printf("Quick Sort, ");
+			}
+
+			printf("%d elements, ", elements);
+			printf("%" PRIu64 " moves, ", stats.moves);
+			printf("%"  PRIu64 " compares\n", stats.compares);
+			
+			for (uint32_t i = 0; i < elements; i++) {
+         		       printf("%13" PRIu32 "\t", A[i]);
+         		       if ((i + 1) % 5 == 0) {
+				       printf("\n");
+                }
+        }
 		}
 	}
 

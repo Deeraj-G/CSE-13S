6d83dd75bc5a11ec4693247bfcc470bffd2e0c38
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-17 02:44:11 +0000

    Fixed heap.c

--- asgn3/heap.c
+++ asgn3/heap.c
@@ -3,28 +3,31 @@
 #include "stats.h"
 #include <inttypes.h>
 #include <stdio.h>
-#include <math.h>
-
+#include <stdbool.h>
 
 uint32_t max_child(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
 	uint32_t left = 2 * first;
 	uint32_t right = left + 1;
 
-	if (right <= last && (cmp(stats, A[right - 1], A[left - 1])) > 0) {
+	if (right <= last && ((cmp(stats, A[right - 1], A[left - 1])) > 0)) {
 		return right;
-	return left;
+	}
+	else {
+		return left;
+	}
 }
 
-uint32_t fix_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
+void fix_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
 	bool found = false;
 	uint32_t mother = first;
-	uint32_t great = max_child(A, mother, last);
+	uint32_t great = max_child(stats, A, mother, last);
 
-	while (mother <= (last / 2) && (!(found))) {
+	while (mother <= (last / 2) && (found == false)) {
 		if ((cmp(stats, A[mother - 1], A[great - 1])) < 0) {
 			swap(stats, &A[mother - 1], &A[great - 1]);
 			mother = great;
-			great = max_child(A, mother, last);
+			move(stats, mother);
+			great = max_child(stats, A, mother, last);
 		}
 		else {
 			found = true;
@@ -32,15 +35,24 @@ uint32_t fix_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
 	}
 }
 
-uint32_t build_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
-
-
-
-
+void build_heap(Stats *stats, uint32_t *A, uint32_t first, uint32_t last) {
+	uint32_t father;
+	for (father = (last / 2); father > first - 1; father -= 1) {
+		fix_heap(stats, A, father, last);
+	}
 }
 
 void heap_sort(Stats *stats, uint32_t *A, uint32_t n) {
+	uint32_t first = 1;
+	uint32_t last = n;
+	uint32_t leaf;
+	
+	build_heap(stats, A, first, last);
 
-
-
+	for (leaf = last; leaf > first; leaf -= 1) {
+		swap(stats, &A[first - 1], &A[leaf - 1]);
+		//move(stats, A[first - 1]);
+		fix_heap(stats, A, first, leaf - 1);
+	}
 }
+

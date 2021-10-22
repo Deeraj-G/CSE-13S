9f9317ca236de22298e77b0c010e2eefc70a4b78
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-22 21:48:50 +0000

    Finished a couple more functions

--- asgn4/graph.c
+++ asgn4/graph.c
@@ -2,8 +2,16 @@
 #include <stdbool.h>
 #include <inttypes.h>
 #include "vertices.h"
+#include "graph.h"
 
-Grap *graph_create(uint32_t vertices, bool undirected) {
+struct Graph {
+     uint32_t vertices;
+     bool undirected;
+     bool verified[VERTICES];
+     uint32_t matrix[VERTICES][VERTICES];
+}
+
+Graph *graph_create(uint32_t vertices, bool undirected) {
 	Graph *G = (Graph *)calloc(1, sizeof(Graph));
 	G->vertices = vertices;
 	G->undirected = undirected;
@@ -11,15 +19,50 @@ Grap *graph_create(uint32_t vertices, bool undirected) {
 }
 
 void graph_delete(Graph **G) {
-	free (*G);
+	free(*G);
 	*G = NULL;
 	return;
 }
 
-struct Graph {
-	uint32_t vertices;
-	bool undirected;
-	bool verified[VERTICES];
-	uint32_t matrix[VERTICES][VERTICES];
+uint32_t graph_vertices(Graph *G) {
+    return G->vertices;    
+}
+
+bool graph_add_edge(Graph *G, uint32_t i, uint32_t j, uint32_t k) {
+    G->matrix[i][j] = k;
+    if (G->undirected == true) {
+        G->matrix[j][i] = k;
+    }
+    
+}
+
+bool graph_has_edge(Graph *G, uint32_t i, uint32_t j) {
+
+
+}
+
+uint32_t graph_edge_weight(Graph *G, uint32_t i, uint32_t j) {
+
+
+}
+
+bool graph_visited(Graph *G, uint32_t v) {
+
+
+}
+
+void graph_mark_visited(Graph *G, uint32_t v) {
+
+
+}
+
+void graph_mark_unvisited(Graph *G, uint32_t v) {
+
+
+}
+
+void graph_print(Graph *G) {
+
+
 }
 

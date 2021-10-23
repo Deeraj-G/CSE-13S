21e08de63df5b135ceabc2611f0860077c5ac7a7
Deeraj Gurram <dgurram@ucsc.edu>
2021-10-23 22:53:49 +0000

    Completed a couple more functions, working on path pop

--- asgn4/path.c
+++ asgn4/path.c
@@ -1,35 +1,75 @@
 #include "path.h"
+#include "stack.h"
+#include "graph.h"
 #include "vertices.h"
-#include <stdio.h>
-#include <stdbool.h>
+
 #include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Path {
+    Stack *vertices;
+    uint32_t length;
+};
 
 Path *path_create(void) {
-
-
+    Path *p = (Path *) calloc(1, sizeof(Path));
+    p->vertices = stack_create(VERTICES);
+    p->length = 0;
+    return p;
 }
 
+
 void path_delete(Path **p) {
-
-
+    if (*p && (*p)->vertices) {
+        free((*p)->vertices);
+        free(*p);
+        *p = NULL;
+    }
+    return;
 }
 
 bool path_push_vertex(Path *p, uint32_t v, Graph *G) {
+    uint32_t x = 0;
+    if (stack_empty(p->vertices) == true) {
+        if (stack_push(p->vertices, v) == true) {
+            p->length = p->length + graph_edge_weight(G, 0, v);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-
+    else {
+        if (stack_peek(p->vertices, x) == true) {
+            stack_push(p->vertices, v);
+            p->length = p->length + graph_edge_weight(G, x, v);
+            return true;
+        }
+        return false;
+    }
 }
 
 bool path_pop_vertex(Path *p, uint32_t *v, Graph *G) {
+    uint32_t x;
+    uint32_t top;
+    if (stack_empty(p->vertices) == true) {
+        return false;
+    }
+    else {
+        top = stack_size(p->vertices);
 
 
 }
 
 uint32_t path_vertices(Path *p) {
-
+    return stack_size(p->vertices);
 }
 
 uint32_t path_length(Path *p) {
-
+    return p->length;
 }
 
 void path_copy(Path *dst, Path *src) {
@@ -39,6 +79,5 @@ void path_copy(Path *dst, Path *src) {
 void path_print(Path *p, FILE *outfile, char *cities[]) {
 
 }
-
 
 

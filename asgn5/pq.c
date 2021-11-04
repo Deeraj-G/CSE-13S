eeebbfed6a1dbd97afb6e86064e881dcd936deeb
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-04 00:37:17 +0000

    fixed min_child function

--- asgn5/pq.c
+++ asgn5/pq.c
@@ -62,50 +62,53 @@ uint32_t pq_size(PriorityQueue *q) {
 
 // Received the following two functions from the stats.c file created by Dr. Long for asgn3
 int cmp(Node *x, Node *y) {
+    
     if (frequency(x) < frequency(y)) {
         return -1;
-    } else if (x > y) {
+    } 
+    else if (frequency(x) > frequency(y)) {
         return 1;
-    } else {
+    } 
+    else {
         return 0;
     }
 }
 
-void swap(Node *x, Node *y) {
-    // FIX SWAP
-    Node *t = x;
+void swap(Node **x, Node **y) {
+    Node *t = *x;
     *x = *y;
-    *y = *t;
+    *y = t;
 }
 
 // Copied parts of my code from heap.c in asgn3 for the following 2 functions
 // This function returns the smaller child between the arguments first and last
-uint32_t min_child(Node *n, PriorityQueue *q, uint32_t first, uint32_t last) {
+uint32_t min_child(PriorityQueue *q, Node *n, uint32_t first, uint32_t last) {
 
     uint32_t left = 2 * first;
     uint32_t right = left + 1;
 
-    // Checks if the right value is >= last and checks if the right value is less than the left value
-    if (right <= last && ((cmp(q->items[right - 1], q->items[left - 1])) > 0)) {
+    // Checks if the right value is >= last and checks if the right value is greater than the left value
+    if (right >= last && ((cmp(q->items[right], q->items[left])) > 0)) {
 
-    //if (right >= last && (A[right - 1] < A[left - 1])) {
-        return right;
+        // If the if statement is fulfilled, the right node is greater, so we should return the left child for min_child
+        return left;
+        
     }   
     
     else {
-        return left;
+        printf("right value is: %d\n", right);
+        return right;
     }   
 } 
 
-
 // This function fixes the heap so it obeys the constraints of a heap after the smallest element has been removed
-void fix_heap(Node *n, PriorityQueue *q, uint32_t first, uint32_t last) {
+void fix_heap(PriorityQueue *q, Node *n, uint32_t first, uint32_t last) {
 
     bool found = false;
     uint32_t mother = first;
     
     // This variable is set equal to the greater value between mother and last
-    uint32_t great = min_child(n, q, mother, last);
+    uint32_t great = min_child(q, n, mother, last);
 
     // This loop checks if the array elements at mother is less than great, and swaps the two elements if it is true
     while (mother <= (last / 2) && (found == false)) {
@@ -114,20 +117,19 @@ void fix_heap(Node *n, PriorityQueue *q, uint32_t first, uint32_t last) {
         if ((cmp(q->items[mother - 1], q->items[great - 1])) < 0) {
         
             // Swap the elements of mother and great
-            swap(q->items[first - 1], q->items[great - 1]);
+            swap(&(q->items[first - 1]), &(q->items[great - 1]));
             
             // Sets mother equal to great
             mother = great;
             
             // Recalculates great now that mother is equal to the previous value of great
-            great = min_child(n, q, mother, last);
+            great = min_child(q, n, mother, last);
         } 
         else {
             found = true;
         }
     }
 }   
-
 
 bool enqueue(PriorityQueue *q, Node *n) {
 
@@ -138,7 +140,7 @@ bool enqueue(PriorityQueue *q, Node *n) {
     else {
         q->items[q->tail] = n;
         q->size += 1;
-        fix_heap(n, q, q->head, q->tail);
+        fix_heap(q, n, q->head, q->tail);
         q->tail += 1;
         return true;
     }

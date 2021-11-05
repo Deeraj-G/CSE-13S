3deda3dd9618353f5489efc1234a17117f433bbf
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-05 00:16:28 +0000

    Finished min_child, fix_heap, and enqueue. They work as expected

--- asgn5/pq.c
+++ asgn5/pq.c
@@ -60,26 +60,6 @@ uint32_t pq_size(PriorityQueue *q) {
     return q->size;
 }
 
-// Received the following two functions from the stats.c file created by Dr. Long for asgn3
-int cmp(Node *x, Node *y) {
-    
-    if (frequency(x) < frequency(y)) {
-        return -1;
-    } 
-    else if (frequency(x) > frequency(y)) {
-        return 1;
-    } 
-    else {
-        return 0;
-    }
-}
-
-void swap(Node **x, Node **y) {
-    Node *t = *x;
-    *x = *y;
-    *y = t;
-}
-
 // Copied parts of my code from heap.c in asgn3 for the following 2 functions
 // This function returns the smaller child between the arguments first and last
 uint32_t min_child(PriorityQueue *q, uint32_t first, uint32_t last) {
@@ -87,8 +67,12 @@ uint32_t min_child(PriorityQueue *q, uint32_t first, uint32_t last) {
     uint32_t left = 2 * first;
     uint32_t right = left + 1;
 
+//    printf("left: %d, frequency: %ld\n", left, frequency(q->items[left - 1]));
+//    printf("right: %d, frequency: %ld\n", right, frequency(q->items[right - 1]));
+
+
     // Checks if the right value is >= last and checks if the right value is greater than the left value
-    if (right <= last && frequency(q->items[right - 1]) < frequency(q->items[left - 1])) {
+    if ((right <= last) && frequency(q->items[right - 1]) < frequency(q->items[left - 1])) {
         return right;
     }   
     
@@ -107,11 +91,10 @@ void fix_heap(PriorityQueue *q, uint32_t first, uint32_t last) {
     uint32_t least = min_child(q, mother, last);
 
     // This loop checks if the array elements at mother is less than least, and swaps the two elements if it is true
-    while (mother <= (last / 2) && (found == false)) {
-        
+    while ((mother <= (last / 2)) && found == false) {
         // Check if the array value at mother is less than the array value at least
         if (frequency(q->items[mother - 1]) > frequency(q->items[least - 1])) {
-            
+        
             // Swap the node values of mother and least
             temp = q->items[mother - 1];
             q->items[mother - 1] = q->items[least - 1];
@@ -121,7 +104,7 @@ void fix_heap(PriorityQueue *q, uint32_t first, uint32_t last) {
             mother = least;
             
             // Recalculates least now that mother is equal to the previous value of least
-            least = min_child(q, mother, last);
+            //least = min_child(q, mother, last);
         } 
         else {
             found = true;
@@ -134,16 +117,21 @@ bool enqueue(PriorityQueue *q, Node *n) {
     if (q->size == q->capacity) {
         return false;
     }
-
     else {
-        q->items[q->tail] = n;
-        q->size += 1;
-        fix_heap(q, q->head, q->tail);
-        q->tail += 1;
-        return true;
+        q->items[q->size] = n;
     }
 
+    q->size += 1;
+    q->head += 1;
+    uint32_t k = q->size;
 
+
+    while (k > 1) {
+        uint32_t parent = k / 2;
+        fix_heap(q, parent, q->size);
+        k = parent;
+    }
+    return true;
 
 }
 

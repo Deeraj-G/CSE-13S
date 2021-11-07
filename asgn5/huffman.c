181543881f74571698cde0e3c76b80c3b79c8c24
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-07 00:49:30 +0000

    Translated python pseudocode for build_codes() to C

--- asgn5/huffman.c
+++ asgn5/huffman.c
@@ -2,11 +2,18 @@
 #include "code.h"
 #include "defines.h"
 #include <stdint.h>
+#include "huffman.h"
+#include <inttypes.h>
+
+#include "node.c"
+#include "pq.c"
 
 // Used TA Eugene's pseudocode for build_tree from his section
+// Used Dr. Long's python pseudocode for build_codes 
 
 Node *build_tree(uint64_t hist[static ALPHABET]) {
     int count;
+
     // Create a priority queue of nodes whose frequency is non-zero
     for (int i = 0; i < ALPHABET; i++) {
         if (hist[i] > 0) {
@@ -24,18 +31,45 @@ Node *build_tree(uint64_t hist[static ALPHABET]) {
     }
     
     while (pq_size(q) > 1) {
-        Node *left = dequeue(q, &n);
-        Node *right = dequeue(q, &n);
+        // Dequeue a node and set Node *left equal to it
+        dequeue(q, &n);
+        Node *left = n;
+        
+        // Dequeue a node and set Node *right equal to it
+        dequeue(q, &n);
+        Node *right = n;
+        
+        // Enqueue the parent node of left and right to q
         Node *parent = node_join(left, right);
         enqueue(q, parent);
     }
 
-    Node *root = dequeue(q, &n);
+    // Dequeue the root node
+    dequeue(q, &n);
+    Node *root = n;
     return root;
 }
 
 void build_codes(Node *root, Code table[static ALPHABET]) {
+    Code c = code_init();
+    uint8_t *bit;
 
+    if (root != 0) {
+        
+        if (!(root->left) && !(root->right)) {
+            table[symbol(root)] = c;
+        }
+
+        else {
+            code_push_bit(c, 0);
+            build_codes(root->left, Code table[static ALPHABET]);
+            code_pop_bit(c, bit);
+
+            code_push_bit(c, 1);
+            build_codes(root->right, Code table[static ALPHABET]);
+            code_pop_bit(c, bit);
+        }
+    }
 }
 
 void dump_tree(int outfile, Node *root) {
@@ -47,5 +81,49 @@ Node *rebuild_tree(uint16_t nbytes, uint8_t tree[static nbytes]) {
 }
 
 void delete_tree(Node **root) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

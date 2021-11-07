7d486cd54f4dc50bb5d56724b9d322e049d8ed74
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-07 22:26:48 +0000

    Checking functionality for pq

--- asgn5/pq.c
+++ asgn5/pq.c
@@ -144,8 +144,8 @@ bool dequeue(PriorityQueue *q, Node **n) {
     }
 
     // Swap the first and last elements in q->items
-    temp2 = q->items[q->size - 1];
-    q->items[q->size - 1] = q->items[q->tail];
+    temp2 = q->items[q->size];
+    q->items[q->size] = q->items[q->tail];
     q->items[q->tail] = temp2;
 
     // Store the dequeued element in the pointer

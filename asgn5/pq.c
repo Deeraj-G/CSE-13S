599a09bba1fc4767e862287ceaf75b58600f0111
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-07 22:35:59 +0000

    Checking pipeline functionality

--- asgn5/pq.c
+++ asgn5/pq.c
@@ -147,9 +147,9 @@ bool dequeue(PriorityQueue *q, Node **n) {
     *n = q->items[q->size];
 
     // Swap the first and last elements in q->items
-    temp2 = q->items[q->size - 1];
-    q->items[q->size - 1] = q->items[0];
-    q->items[0] = temp2;
+    temp2 = q->items[0];
+    q->items[0] = q->items[q->size - 1];
+    q->items[q->size - 1] = temp2;
 
     // Decrement the size to effectively get rid of the dequeued element
     q->size -= 1;

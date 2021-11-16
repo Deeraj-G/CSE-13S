64374e9d2f4426fac2357ca47f9699c8cb1d5215
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-16 00:32:15 +0000

    Fixed some bugs with my makefile, still running into some errors

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -8,29 +8,46 @@ void gcd(mpz_t d, mpz_t a, mpz_t b) {
     
     while (b != 0) {
 
-        // Set d equal to b
-        mpz_add(d, 0, b);
+        // Initialize an mpz_t variable
+        mpz_t t;
+        mpz_init(t);
+
+        // Set t equal to b
+        mpz_add(t, 0, b);
 
         // Set b equal to a mod b
         mpz_mod(b, a, b);
 
-        // Set a equal to d
+        // Set a equal to t
         mpz_add(a, 0, t);
     }
 
-    return a;
+    // Set d equal to a
+    d = a;
 }
-
+/*
 void mod_inverse(mpz_t i, mpz_t a, mpz_t n) {
 
 }
-
+*/
 void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
+
+    // Initialize a mpz_t of value 0
+    mpz_t zero;
+    mpz_init(zero);
+
+    // Initialize a mpz_t of value 1
+    mpz_t one;
+    mpz_init(one);
+
+    // Initialize an mpz_t of value 2
+    mpz_t two;
+    mpz_init(two);
 
     // Set v equal to 1
     mpz_t v;
     mpz_init(v);
-    mpz_add(v, 0, 1);
+    mpz_add(v, 0, one);
 
     // Set p equal to the base
     mpz_t p;
@@ -38,25 +55,26 @@ void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
     mpz_add(p, 0, base);
 
     // While loop
-    while (exponent > 0) {
+    while (exponent > zero) {
         
+        // Check if exponent is odd
         if (exponent % 2 == 1) {
             // Set v equal to (v*p) mod n
             mpz_mul(v, v, p);
-            mpz_mod(v, v, n);
+            mpz_mod(v, v, modulus);
         }
 
         // Set p equal to (p*p) mod n
         mpz_mul(p, p, p);
-        mpz_mod(p, p, n);
-        
+        mpz_mod(p, p, modulus);
+
         // Set d equal to d / 2
-        mpz_fdiv_q(d, d, 2);
+        mpz_fdiv_q(exponent, exponent, two);
     }
 
-    return v;
+    out = v;
 }
-
+/*
 bool is_prime(mpz_t n, uint64_t iters) {
 
 }
@@ -64,3 +82,4 @@ bool is_prime(mpz_t n, uint64_t iters) {
 void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
 
 }
+*/

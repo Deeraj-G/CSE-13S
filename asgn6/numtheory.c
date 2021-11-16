7e76dc76bf4fd0672f4f36a2f49378e7c878f304
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-16 07:13:45 +0000

    Makefile gives no errors, starting the testing process

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -1,9 +1,10 @@
+#include <gmp.h>
+#include "numtheory.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <gmp.h>
-#include "numtheory.h"
 #include <inttypes.h>
+#include "randstate.h"
 
 // Got most of is_prime from TA Eric Hernandez's section
 
@@ -23,6 +24,8 @@ void gcd(mpz_t d, mpz_t a, mpz_t b) {
 
         // Set a to t
         mpz_set(a, t);
+
+        mpz_clear(t);
     }
 
     // Set d equal to a
@@ -70,34 +73,33 @@ void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus) {
 
         // Set d equal to d / 2
         mpz_fdiv_q_ui(exponent, exponent, 2);
+
+        mpz_clear(temp);
     }
 
+    mpz_clears(zero, v, p, NULL);
     out = v;
 }
 
 // Got most of is_prime from TA Eric Hernandez's section
 bool is_prime(mpz_t n, uint64_t iters) {
 
-    // Bit shifting for the n-1 = (2^s)r
-    // Initialize mpz's for the bit shifting
-    mpz_t n_min_one, two, r;
-    mpz_inits(n_min_one, two, r, NULL);
+    // Initialize mpz's
+    mpz_t n_min_one, two, r, a, upper_bound, y;
+    mpz_inits(n_min_one, two, r, a, upper_bound, y, NULL);
 
-    // Put a comment here
+    // Bit shifting for the n-1 = (2^s)r
     mpz_sub_ui(n_min_one, n, 1);
     mpz_set_str(two, "2", 10);
 
     mp_bitcnt_t s = 2;
+
     while (mpz_divisible_2exp_p(n_min_one, s)) {
         s++;
     }
     s--;
 
     mpz_tdiv_q_2exp(r, n_min_one, s);
-
-    // Initialize mpz's for the for loop
-    mpz_t a, upper_bound, y, j;
-    mpz_inits(a, upper_bound, y, j);
 
     for (uint64_t i = 0; i < iters; i++) {
         
@@ -109,15 +111,14 @@ bool is_prime(mpz_t n, uint64_t iters) {
         if ((mpz_cmp_ui(y, 1) != 0) && mpz_cmp(n_min_one, y) != 0) {
 
             // Set j to 1
+            mpz_t j;
+            mpz_init(j);
             mpz_set_ui(j, 1);
 
             // Set sdec equal to s-1
             mp_bitcnt_t sdec = s - 1;
 
-            // Set ndec equal to n-1
-            mp_bitcnt_t ndec = n - 1;
-
-            while ((mpz_cmp(j, sdec) <= 0) && (mpz_cmp(y, ndec) != 0)) {
+            while ((mpz_cmp_ui(j, sdec) <= 0) && (mpz_cmp(y, n_min_one) != 0)) {
 
                 // Set y equal to power_mod(y,2,n)
                 pow_mod(y, y, two, n);
@@ -131,12 +132,21 @@ bool is_prime(mpz_t n, uint64_t iters) {
             if (mpz_cmp(y, n-1) != 0) {
                 return false;
             }
+
+//            mpz_clears(sdec, ndec, NULL);
         }
     }
+    mpz_clears(n_min_one, two, r, a, upper_bound, y, NULL);
     return true;
 }
-/*
-void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
 
+void make_prime(mpz_t p, uint64_t bits, uint64_t iters) {
+    mpz_set_ui(p, 3);
+    bits = 0;
+    iters = 0;
 }
-*/
+
+int main() {
+    uint64_t seed = 2021;
+    randstate_init(seed);
+}

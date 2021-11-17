be08f8ea42778cc5b48c70af7531e6b4b71dfd40
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-17 04:53:40 +0000

    Finished rsa_make_pub, put in random commands for other functions for make to work

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -32,37 +32,70 @@ void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t i
     mpz_set(e, gcd_e_n);
     mpz_clears(p_min_one, q_min_one, gcd_e_n, NULL);
 }
-/*
+
 void rsa_write_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
+    if (username) {
+        mpz_set(n, e);
+    }
+    if (pbfile) {
+        mpz_set(s, n);
+    }
 }
 
 void rsa_read_pub(mpz_t n, mpz_t e, mpz_t s, char username[], FILE *pbfile) {
+    if (username) {
+        mpz_set(n, e);
+    }
+    if (pbfile) {
+        mpz_set(s, n);
+    }
 }
 
 void rsa_make_priv(mpz_t d, mpz_t e, mpz_t p, mpz_t q) {
+    mpz_set(d, e);
+    mpz_set(p, q);
 }
 
 void rsa_write_priv(mpz_t n, mpz_t d, FILE *pvfile) {
+    if (pvfile) {
+        mpz_set(n, d);
+    }
 }
 
 void rsa_read_priv(mpz_t n, mpz_t d, FILE *pvfile) {
+    if (pvfile) {
+        mpz_set(n, d);
+    }
 }
 
 void rsa_encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n) {
+    mpz_set(c, m);
+    mpz_set(e, n);
 }
 
 void rsa_encrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t e) {
+    if (infile && outfile) {
+        mpz_set(n, e);
+    }
 }
 
 void rsa_decrypt(mpz_t m, mpz_t c, mpz_t d, mpz_t n) {
+    mpz_set(m, c);
+    mpz_set(d, n);
 }
 
 void rsa_decrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t d) {
+    if (infile && outfile) {
+        mpz_set(n, d);
+    }
 }
 
 void rsa_sign(mpz_t s, mpz_t m, mpz_t d, mpz_t n) {
+    mpz_set(s, m);
+    mpz_set(d, n);
 }
 bool rsa_verify(mpz_t m, mpz_t s, mpz_t e, mpz_t n) {
-
+    mpz_set(m, s);
+    mpz_set(e, n);
+    return false;
 }
-*/

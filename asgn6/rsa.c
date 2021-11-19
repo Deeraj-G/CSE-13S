ba514c752a389c8e622c39766d3d2e511b4b4bb2
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-19 02:42:00 +0000

    make_keys

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -12,14 +12,16 @@
 // Used TA Eric Hernandez's pseudocode for this funcion
 void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t iters) {
 
-    mpz_t p_min_one, q_min_one, gcd_e_n;
-    mpz_inits(p_min_one, q_min_one, gcd_e_n, NULL);
+    mpz_t p_min_one, q_min_one, gcd_e_n, one;
+    mpz_inits(p_min_one, q_min_one, gcd_e_n, one, NULL);
 
     uint64_t pbits = (random() % (nbits / 2)) + (nbits / 4);
     uint64_t qbits = nbits - pbits;
 
     make_prime(p, pbits + 1, iters);
     make_prime(q, qbits + 1, iters);
+
+    mpz_set_ui(one, 1);
 
     mpz_sub_ui(p_min_one, p, 1);
     mpz_sub_ui(q_min_one, q, 1);
@@ -30,7 +32,7 @@ void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t i
     do {
         mpz_urandomb(e, state, en);
         gcd(gcd_e_n, e, n);
-    } while (mpz_cmp_ui(gcd_e_n, 1) > 0);
+    } while (mpz_cmp(gcd_e_n, one) > 0);
 
     mpz_set(e, gcd_e_n);
     mpz_clears(p_min_one, q_min_one, gcd_e_n, NULL);
@@ -136,7 +138,7 @@ void rsa_encrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t e) {
     // If the return value != k-1, it means an error occurred or the EOF was reached
     while (feof(infile) == 0) {
 
-        // 
+        // Set j equal to the number of bytes read
         j = fread(array + 1, sizeof(uint8_t), k - 1, infile);
 
         // Convert the read bytes into an mpz_t m

37d3b1e7da2705bff3d29b097c3a3fce649aa9c2
Deeraj Gurram <dgurram@ucsc.edu>
2021-11-29 03:38:40 +0000

    Made some changes to bf.c and ht.c, added skeleton files for the rest

--- asgn7/bf.c
+++ asgn7/bf.c
@@ -10,16 +10,16 @@ struct BloomFilter {
     uint64_t primary [2]; // Primary hash function salt .
     uint64_t secondary [2]; // Secondary hash function salt .
     uint64_t tertiary [2]; // Tertiary hash function salt .
-    BitVector * filter ;
+    BitVector *filter;
 };
 
 BloomFilter *bf_create(uint32_t size) {
     BloomFilter *bf = (BloomFilter *) malloc(sizeof(BloomFilter));
-
     bf->primary[0] = SALT_PRIMARY_LO;
     bf->primary[1] = SALT_PRIMARY_HI;
     bf->secondary[0] = SALT_SECONDARY_LO;
     bf->secondary[1] = SALT_SECONDARY_HI;
     bf->tertiary[0] = SALT_HASHTABLE_LO;
     bf->tertiary[1] = SALT_HASHTABLE_HI;
+    *filter = bv_create(size);
 }
--- asgn7/ht.c
+++ asgn7/ht.c
@@ -11,3 +11,11 @@ struct HashTable {
     uint32_t size ;
     Node ** trees ;
 };
+
+HashTable *ht_create(uint32_t size) {
+    HashTable *ht = (HashTable *) malloc(sizeof(HashTable));
+    ht->salt[0] = SALT_HASHTABLE_LO;
+    ht->salt[1] = SALT_HASHTABLE_HI;
+    // Create a Node *trees
+}
+

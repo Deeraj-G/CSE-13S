bfc47d6ab913238cbea56de037d4eded7eb8986b
Deeraj Gurram <dgurram@ucsc.edu>
2021-12-02 07:38:29 +0000

    Translated tutor Eric's pseudocode to C

---
+++ asgn7/bst.c
@@ -0,0 +1,62 @@
+#include "node.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define max(x, y) x > y ? x : y
+
+// Got most of bst.c from tutor Eric Hernandez
+
+extern uint64_t branches;
+
+Node *bst_create(void) {
+    return NULL;
+}
+
+uint32_t bst_height(Node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return max(bst_height(root->left), bst_height(root->right)) + 1;
+}
+
+uint32_t bst_size(Node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    return bst_size(root->left) + bst_size(root->right) + 1;
+}
+
+Node *bst_find(Node *root, char *oldspeak) {
+    Node *curr = root;
+    if (root != NULL && oldspeak != NULL) {
+        while (curr != NULL && strcmp(curr->oldspeak, oldspeak) != 0) {
+            if (strcmp(curr->oldspeak, oldspeak) > 0) {
+                curr = curr->left;
+            } else {
+                curr = curr->right;
+            }
+        }
+    }
+    return curr;
+}
+/*
+Node *bst_insert(Node *root, char *oldspeak, char *newspeak) {
+}
+
+void bst_print(Node *root) {
+}
+*/
+void bst_delete(Node **root) {
+    if (&(*root) != NULL) {
+        if ((*root)->left != NULL) {
+            bst_delete(&(*root)->left);
+        }
+        if ((*root)->right != NULL) {
+            bst_delete(&(*root)->right);
+        }
+        node_delete(&(*root));
+    }
+}

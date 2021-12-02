83871b21341a3600dbb9fce4f85a84e9123db497
Deeraj Gurram <dgurram@ucsc.edu>
2021-12-02 07:39:06 +0000

    Finished half of ht.c, still need to do a few functions and test them

--- asgn7/ht.c
+++ asgn7/ht.c
@@ -1,9 +1,12 @@
 #include "bst.h"
 #include "ht.h"
+#include "salts.h"
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 #include <stdlib.h>
+
+// Used tutor Eric Hernandez's pseudocode for most of ht.c
 
 struct HashTable {
     uint64_t salt[2];
@@ -11,9 +14,56 @@ struct HashTable {
     Node **trees;
 };
 
+// Got most of this function from tutor Eric Hernandez
 HashTable *ht_create(uint32_t size) {
     HashTable *ht = (HashTable *) malloc(sizeof(HashTable));
     ht->salt[0] = SALT_HASHTABLE_LO;
     ht->salt[1] = SALT_HASHTABLE_HI;
-    // Create a Node *trees
+    ht->trees = (Node **) calloc(size, sizeof(Node *));
+    ht->size = size;
+    return ht;
 }
+
+// Got this function from tutor Eric Hernandez
+void ht_delete(HashTable **ht) {
+    for (uint32_t i = 0; i < (*ht)->size; i++) {
+        if ((*ht)->trees[i] != NULL) {
+            bst_delete(&(*ht)->trees[i]);
+        }
+    }
+    free((*ht)->trees);
+    free(*ht);
+    *ht = NULL;
+}
+
+uint32_t ht_size(HashTable *ht) {
+    return ht->size;
+}
+
+/*
+// Got this function from tutor Eric Hernandez
+Node *ht_lookup(HashTable *ht, char *oldspeak) {
+    
+
+}
+
+// Got this function from tutor Eric Hernandez
+void ht_insert(HashTable *ht, char *oldspeak, char *newspeak) {
+}
+
+// Got this function from tutor Eric Hernandez
+uint32_t ht_count(HashTable *ht) {
+}
+
+// Got this function from tutor Eric Hernandez
+uint32_t ht_count(HashTable *ht) {
+}
+
+// Got this function from tutor Eric Hernandez
+double ht_avg_bst_size(HashTable *ht) {
+}
+
+
+void ht_print(HashTable *ht) {
+}
+*/

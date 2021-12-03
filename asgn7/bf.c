2f1819e382550c3f2fbb6cbf6203e956d2f2ac01
Deeraj Gurram <dgurram@ucsc.edu>
2021-12-03 01:28:12 +0000

    Finished most of bf.c

--- asgn7/bf.c
+++ asgn7/bf.c
@@ -6,6 +6,8 @@
 #include <inttypes.h>
 #include <stdlib.h>
 
+// Got everything except bf_create from tutor Eric Hernandez
+
 struct BloomFilter {
     uint64_t primary[2]; // Primary hash function salt .
     uint64_t secondary[2]; // Secondary hash function salt .
@@ -24,3 +26,25 @@ BloomFilter *bf_create(uint32_t size) {
     bf->filter = bv_create(size);
     return bf;
 }
+
+void bf_delete(BloofFilter **bf) {
+    bv_delete(&(*bf)->fileter);
+    free(*bf);
+    *bf = NULL;
+}
+
+uint32_t bf_size(BloomFilter *bf) {
+    return bv_length(bf->filter);
+}
+
+void bf_insert(BloomFilter *bf, char *oldspeak) {
+    bv_set_bit(bf->filter, hash(bf->primary, oldspeak) % bf_size(bf));
+    bv_set_bit(bf->filter, hash(bf->secondary, oldspeak) % bf_size(bf));
+    bv_set_bit(bf->filter, hash(bf->tertiary, oldspeak) % bf_size(bf));
+}
+
+bool bf_probe(BloomFilter *bf, char *oldspeak) {
+    
+}
+
+
